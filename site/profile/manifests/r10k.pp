8c2ae6579408c6340f51e60d19645a3603678aaa
mohdassen <46775336+mohdassen@users.noreply.github.com>
2019-02-12 11:30:01 +0300

    Update r10k.pp

--- site/profile/manifests/r10k.pp
+++ site/profile/manifests/r10k.pp
@@ -1,6 +1,6 @@
 class profile::r10k {
   class {'r10k':
-    remote => 'https://github.com/joshsamuelson/control-repo',
+    remote => 'https://github.com/mohdassen/control_repo_puppet.git',
   }
   class {'r10k::webhook::config':
     use_mcollective => false,
